5e80ebf86f0c9a2aae2d9f01d8edbe01bdcf9c97
Ali Imran <IMRAN@pk.ibm.com>
2021-02-03 22:50:06 +0500

    added login validation

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -19,29 +19,69 @@ class LoginComponent extends Component {
     constructor(props){
         super(props)
         this.state = {
-            username: 'Enter user name',
-            passwrod: ''
+            username: '',
+            password: '',
+            loginSuccess: false,
+            loginFailed: false
         }
 
-        this.handeUsernameChange = this.handeUsernameChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+
     }
 
-    handeUsernameChange(event){
-        this.setState({username:event.target.value});
-        console.log(event.target.value)
+    handleChange(event){
+        console.log(this.state)
+        this.setState(
+            {
+                [event.target.name]:event.target.value
+            });
     }
 
-    handepasswordChange(event){
-        this.setState({password:event.target.value});
+    loginClicked()
+    {
+        if (this.state.username === 'user1' && this.state.password ==='pwd'){
+            this.setState({loginSuccess: true,loginFailed: false})
+
+        }
+        else
+        {
+            this.setState({loginSuccess: false,loginFailed: true})
+        }
+
     }
+
     render (){
         return (
             <div className="toDo">
-               User Name: <input type="text" name = "username" value={this.state.username}  onChange={this.handeUsernameChange}></input>
-               Password: <input type="password" name = "password" value={this.state.passwrod} onChange={this.handePasswordChange} ></input>
-               <button>Login</button>
+               User Name: <input type="text" name = "username" value={this.state.username}  onChange={this.handleChange}></input>
+               Password: <input type="password" name = "password" value={this.state.password} onChange={this.handleChange} ></input>
+               <button onClick={this.loginClicked} >Login</button>
+               <ShowSuccessMessage loginSuccess={this.state.loginSuccess} />
+               <ShowFailedessage loginFailed={this.state.loginFailed} />
             </div>
           );
+    }
+
+}
+
+function  ShowSuccessMessage(props){
+
+    if (props.loginSuccess){
+         return <div>Login Success</div>
+    }
+    else {
+        return null
+    }
+}
+
+function  ShowFailedessage(props){
+
+    if (props.loginFailed){
+         return <div>Login Failed</div>
+    }
+    else {
+        return null
     }
 }
 
